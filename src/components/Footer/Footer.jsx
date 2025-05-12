3d1c05ca8931700c4236743336e59b1b8063f475
mohamedsarwat5 <mohamedelsayed5897@gmail.com>
2025-05-12 14:28:30 +0300

    dad

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,7 +9,7 @@ export default function Footer() {
 
 
 
-            <div className='bg-[#0F0C12] w-full overflow-hidden h-16 border-t border-btn-purple px-3'>
+            <div className='bg-[#0F0C12] w-full overflow-hidden h-16 border-t border-btn-purple px-2'>
                 <div className='flex items-center justify-center h-full'>
                     <h2 className='text-white'>©Copyright 2025 - <span className='font-semibold lg:text-[19px]'>Mayar Sakr</span> - All Rights Reserved</h2>
                 </div>
